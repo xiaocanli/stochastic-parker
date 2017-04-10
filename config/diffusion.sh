2b500082f0f15582099a8f9d853fbc3900a71319
Xiaocan Li <phyxiaolee@gmail.com>
2017-04-10 17:33:18 -0600

    Update job running script for travis CI testing

--- config/diffusion.sh
+++ config/diffusion.sh
@@ -2,7 +2,7 @@
 
 conf=conf.dat
 mpi_size=64
-nptl=10000
+nptl=100
 ts=0
 te=210
 dist_flag=1 # 0 for Maxwellian. 1 for delta function.
@@ -16,7 +16,8 @@ run_stochastic () {
     change_variable mag_dependency $3
     # cat $conf
     cd ..
-    mpirun -np $mpi_size ./stochastic-2dmhd.exec -dm $4 -np $nptl -ts $ts -te $te -df $dist_flag
+    # mpirun -np $mpi_size ./stochastic-2dmhd.exec -dm $4 -np $nptl -ts $ts -te $te -df $dist_flag
+    srun -n $mpi_size ./stochastic-2dmhd.exec -dm $4 -np $nptl -ts $ts -te $te -df $dist_flag
     mkdir -p data/$5/$6
     mv data/*dat data/$5/$6
     cd config
@@ -26,16 +27,19 @@ run_stochastic () {
 }
 
 stochastic () {
-    run_stochastic 0 0.0 0 $1 $2 p000_b000
-    run_stochastic 1 1.0 0 $1 $2 p100_b000
-    run_stochastic 1 1.3333333 0 $1 $2 p133_b000
+    # run_stochastic 0 0.0 0 $1 $2 p000_b000
+    # run_stochastic 1 1.0 0 $1 $2 p100_b000
+    # run_stochastic 1 1.3333333 0 $1 $2 p133_b000
     # run_stochastic 1 1.0 1 $1 $2 p100_b100
-    # run_stochastic 1 1.3333333 1 $1 $2 p133_b100
+    run_stochastic 1 1.3333333 1 $1 $2 p133_b100
 }
 
-mhd_run_dir=/net/scratch3/xiaocanli/mhd/S1E5/beta001/
-run_name=S16E5_beta001_bg0_new
-stochastic $mhd_run_dir $run_name
+# mhd_run_dir=/net/scratch3/xiaocanli/mhd/S1E5/beta001/
+# run_name=S16E5_beta001_bg0_new
+# stochastic $mhd_run_dir $run_name
 # mhd_run_dir=/net/scratch3/xiaocanli/mhd/S1E5/beta01/
 # run_name=S16E5_beta01_bg0
 # stochastic $mhd_run_dir $run_name
+mhd_run_dir=/net/scratch3/xiaocanli/mhd/S16E5/beta001_bg10/bin_data/
+run_name=S1E4_beta001_bg05
+stochastic $mhd_run_dir $run_name
