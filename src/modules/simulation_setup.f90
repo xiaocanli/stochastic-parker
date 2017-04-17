f69e5c40cd5ed97c066a0f51b537799a39029a2b
Xiaocan Li <phyxiaolee@gmail.com>
2017-04-17 09:38:33 -0600

    Add spatial range of the field data

--- src/modules/simulation_setup.f90
+++ src/modules/simulation_setup.f90
@@ -18,7 +18,10 @@ module simulation_setup_module
     type field_configuration
         integer :: nxg, nyg, nzg    ! Number of grids with ghost cells
         integer :: nxf, nyf, nzf
-        integer :: ix_min, ix_max, iy_min, iy_max, iz_min, iz_max
+        integer :: ix_min, ix_max, iy_min
+        integer :: iy_max, iz_min, iz_max
+        real(dp) :: xmin, ymin, zmin
+        real(dp) :: xmax, ymax, zmax
     end type field_configuration
 
     type(field_configuration) :: fconfig
@@ -146,21 +149,33 @@ module simulation_setup_module
 
             call set_data_boundaries(ix, mpi_sizex, nx, mhd_config%nx, &
                                      fconfig%ix_min, fconfig%ix_max)
+            fconfig%xmin = (ix - 0.5) * mhd_config%dx
+            fconfig%xmax = fconfig%xmin + nx * mhd_config%dx
             if (ndim > 1) then
                 call set_data_boundaries(iy, mpi_sizey, ny, mhd_config%ny, &
                                          fconfig%iy_min, fconfig%iy_max)
+                fconfig%ymin = (iy - 0.5) * mhd_config%dy
+                fconfig%ymax = fconfig%ymin + ny * mhd_config%dy
                 if (ndim > 2) then
                     call set_data_boundaries(iz, mpi_sizez, nz, mhd_config%nz, &
                                              fconfig%iz_min, fconfig%iz_max)
+                    fconfig%zmin = (iz - 0.5) * mhd_config%dz
+                    fconfig%zmax = fconfig%zmin + nz * mhd_config%dz
                 else
                     fconfig%iz_min = 1
                     fconfig%iz_max = 1
+                    fconfig%zmin = mhd_config%zmin
+                    fconfig%zmax = mhd_config%zmax
                 endif
             else
                 fconfig%iy_min = 1
                 fconfig%iy_max = 1
                 fconfig%iz_min = 1
                 fconfig%iz_max = 1
+                fconfig%ymin = mhd_config%ymin
+                fconfig%ymax = mhd_config%ymax
+                fconfig%zmin = mhd_config%zmin
+                fconfig%zmax = mhd_config%zmax
             endif
 
             fconfig%nxf = fconfig%ix_max - fconfig%ix_min + 1
@@ -189,6 +204,14 @@ module simulation_setup_module
                 fconfig%iz_max = mhd_config%nz
                 fconfig%nzf = mhd_config%nz
             endif
+
+            fconfig%xmin = mhd_config%xmin
+            fconfig%ymin = mhd_config%ymin
+            fconfig%zmin = mhd_config%zmin
+
+            fconfig%xmax = mhd_config%xmax
+            fconfig%ymax = mhd_config%ymax
+            fconfig%zmax = mhd_config%zmax
         endif
     end subroutine set_field_configuration
 
