903c35d45161a5d239836d68c25f8d8d06d4629c
Xiaocan Li <phyxiaolee@gmail.com>
2017-05-06 15:25:10 -0600

    Update CMake files

--- distclean.cmake
+++ distclean.cmake
@@ -4,11 +4,14 @@
 # We want to start from the top of the source dir, so if we are in build
 # we want to start one directory up
 GET_FILENAME_COMPONENT(BASEDIR ${CMAKE_SOURCE_DIR} NAME)
-IF(${BASEDIR} STREQUAL "build")
+
+IF(${BASEDIR} MATCHES "build*")
     SET(TOPDIR "${CMAKE_SOURCE_DIR}/..")
 ELSE()
     SET(TOPDIR "${CMAKE_SOURCE_DIR}")
 ENDIF()
+
+LIST(REMOVE_ITEM TOPDIR ".git")
 
 MACRO(GET_PARENT_DIRECTORIES search_string return_list grandparents)
     FILE(GLOB_RECURSE new_list ${search_string})
@@ -29,17 +32,17 @@ MACRO(GET_PARENT_DIRECTORIES search_string return_list grandparents)
 ENDMACRO()
 
 # Find directories and files that we will want to remove
-FILE(GLOB_RECURSE CMAKECACHE "${TOPDIR}/*CMakeCache.txt")
-FILE(GLOB_RECURSE CMAKEINSTALL "${TOPDIR}/*cmake_install.cmake"
-                               "${TOPDIR}/*install_manifest.txt")
-FILE(GLOB_RECURSE MAKEFILE "${TOPDIR}/*Makefile")
-FILE(GLOB_RECURSE CMAKETESTFILES "${TOPDIR}/*CTestTestfile.cmake")
+# This assumes that we are in build* directory
+FILE(GLOB_RECURSE CMAKECACHE "${BASEDIR}/*CMakeCache.txt")
+FILE(GLOB_RECURSE CMAKEINSTALL "${BASEDIR}/*cmake_install.cmake"
+                               "${BASEDIR}/*install_manifest.txt")
+FILE(GLOB_RECURSE MAKEFILE "${BASEDIR}/*Makefile")
+FILE(GLOB_RECURSE CMAKETESTFILES "${BASEDIR}/*CTestTestfile.cmake")
+FILE(GLOB EXECUTABLES "${TOPDIR}/*exec")
 SET(TOPDIRECTORIES "${TOPDIR}/lib" 
                    "${TOPDIR}/test"
                    "${TOPDIR}/bin"
 )
-LIST(REMOVE_ITEM MAKEFILE "${TOPDIR}/vpic-sorter/Makefile"
-    "${TOPDIR}/python/colormap/Makefile" "${TOPDIR}/particle_tracer/Makefile")
 
 # CMake has trouble finding directories recursively, so locate these
 # files and then save the parent directory of the files
@@ -48,6 +51,7 @@ GET_PARENT_DIRECTORIES(LastTest.log CMAKETESTING 1)
 
 # Place these files and directories into a list
 SET(DEL ${TOPDIRECTORIES}
+        ${EXECUTABLES}
         ${CMAKECACHE}
         ${CMAKEINSTALL}
         ${MAKEFILE}
@@ -57,8 +61,8 @@ SET(DEL ${TOPDIRECTORIES}
 )
 
 # If we are not in the build dir, delete that as well
-IF(NOT (${BASEDIR} STREQUAL "build"))
-    FILE(GLOB BUILD "${TOPDIR}/build")
+IF(NOT (${BASEDIR} MATCHES "build*"))
+    FILE(GLOB BUILD "${TOPDIR}/build*")
     SET(DEL ${DEL} ${BUILD})
 ENDIF()
 
