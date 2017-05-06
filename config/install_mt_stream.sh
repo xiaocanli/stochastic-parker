f6f5275955133679759c022bdea271989c76dbaf
Xiaocan Li <phyxiaolee@gmail.com>
2017-05-06 15:25:32 -0600

    Change mt_stream default install directory

--- config/install_mt_stream.sh
+++ config/install_mt_stream.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ -f mt_stream/mt_stream.o ]; then
+build_dir=$PWD
+
+if [ -f $HOME/.local/mt_stream/mt_stream.o ]; then
     echo "mt_stream.o found -- nothing to build."
 else
-    mkdir mt_stream
-    cd mt_stream
+    mkdir $HOME/.local/mt_stream
+    cd $HOME/.local/mt_stream
     wget http://theo.phys.sci.hiroshima-u.ac.jp/~ishikawa/PRNG/mt_stream_f90.tar.gz
     tar zxvf mt_stream_f90.tar.gz
     cd mt_stream_f90-1.11
@@ -14,5 +16,6 @@ else
     make
     cd ../../
     test "$?BASH_VERSION" = "0" || eval 'setenv() { export "$1=$2"; }'
-    setenv MT_STREAM ${PWD}/mt_stream/mt_stream_f90-1.11
+    setenv MT_STREAM_GCC ${PWD}/mt_stream/mt_stream_f90-1.11
+    cd $build_dir
 fi
