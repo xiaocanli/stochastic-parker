b188819fc1160d0ca74ca2d7889a9c311d187627
Xiaocan Li <phyxiaolee@gmail.com>
2017-11-29 20:15:07 -0700

    Update modules for grizzly

--- config/module_lanl_grizzly.sh
+++ config/module_lanl_grizzly.sh
@@ -1,7 +1,6 @@
 #Modules required for LANL Grizzly
-module load intel/16.0.3
-module load mvapich2
+module load intel openmpi
 module load friendly-testing
 module load cmake
 module load git
-module load hdf5-parallel/1.8.16
+module load hdf5-parallel
