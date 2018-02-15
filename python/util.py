4e3ee6a7bb58fe22e3a958cb6e14ecdc25e4bfb8
Xiaocan Li <phyxiaolee@gmail.com>
2018-02-15 10:33:06 -0700

    Add module for utility function

--- python/util.py
+++ python/util.py
@@ -1,7 +1,14 @@
+"""
+Utility functions
+"""
 import errno
 import os
 
+import numpy as np
+
 def mkdir_p(path):
+    """Create directory recursively
+    """
     try:
         os.makedirs(path)
     except OSError as exc:  # Python >2.5
@@ -9,3 +16,24 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+def load_mhd_config(run_dir):
+    """Load MHD simulation configuration
+    """
+    fname = run_dir + 'bin_data/mhd_config.dat'
+    mtype = np.dtype([('dx', float), ('dy', float), ('dz', float),
+                      ('xmin', float), ('ymin', float), ('zmin', float),
+                      ('xmax', float), ('ymax', float), ('zmax', float),
+                      ('lx', float), ('ly', float), ('lz', float),
+                      ('dt_out', float),
+                      ('nx', np.int32), ('ny', np.int32), ('nz', np.int32),
+                      ('nxs', np.int32), ('nys', np.int32), ('nzs', np.int32),
+                      ('topox', np.int32), ('topoy', np.int32), ('topoz', np.int32),
+                      ('nvar', np.int32),
+                      ('bcx', np.int32), ('bcy', np.int32), ('bcz', np.int32)])
+    mhd_config = np.fromfile(fname, dtype=mtype)
+    return mhd_config
+
+
+if __name__ == "__main__":
+    pass
