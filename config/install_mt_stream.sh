04120fa762b83cadcaf43c66883eeac673ea821b
Xiaocan Li <4249978+xiaocanli@users.noreply.github.com>
2023-11-18 22:01:11 -0500

    fix error when compiling mt_stream

--- config/install_mt_stream.sh
+++ config/install_mt_stream.sh
@@ -13,6 +13,7 @@ else
     sed -i -e 's/ifort/gfortran/g' Makefile
     sed -i -e 's/icpc/g++/g' Makefile
     sed -i -e 's/FFLAGS := -O3 -g -traceback/FFLAGS := -O3 -g -fno-range-check/g' Makefile
+    sed -i -e 's/Z'\''123'\'', Z'\''234'\'', Z'\''345'\'', Z'\''456'\''/int\(Z'\''123'\''\), int\(Z'\''234'\''\), int\(Z'\''345'\''\), int\(Z'\''456'\''\)/g' check_stream.F90
     make
     cd ../../
     test "$?BASH_VERSION" = "0" || eval 'setenv() { export "$1=$2"; }'
