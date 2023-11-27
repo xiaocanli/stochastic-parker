e4bb06698fddba29ae6ebc85abd20b213b8ab6c5
Xiaocan Li <4249978+xiaocanli@users.noreply.github.com>
2023-11-27 13:23:49 -0500

    install the requirements [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'Global Particle Acceleration and Transport (GPAT) Model'
 copyright = '2015-2022, Xiaocan Li'
 author = 'Xiaocan Li'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.2'
+version = '0.2.0'
 
 # -- General configuration
 
